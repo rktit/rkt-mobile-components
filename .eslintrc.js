f90e4cf5da105760817c647f374ffef147868802
Luiz Ricardo <luiz.constantino@redspark.io>
2024-06-05 22:50:10 -0300

    fix: ajuste de inputs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-    ],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
