7f4ed810755b2bf4275ee25d4e63041b9ef734c3
Luiz Ricardo <luiz.constantino@redspark.io>
2024-06-06 12:28:06 -0300

    fix: update do yarn e build para android

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,6 @@ module.exports = {
   extends: ['plugin:@typescript-eslint/recommended'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
